b2eb5534c772ca0a7295d01450fee94e5e304673
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-02-02 17:00:57 +0530

    Add files via upload

--- heroku.sh
+++ heroku.sh
@@ -1,11 +1,38 @@
 hs() {
-cd $PREFIX/bin
 apt autoremove nodejs
 apt update
 apt upgrade
 apt install --fix-broken
 apt install nodejs || apt reinstall nodejs
-npm install -g heroku
-heroku login -i
 }
 hs
+if [ "$(node --version)" == "v17.4.0" ];then
+echo
+else
+hs
+fi
+inh() {
+printf "\n Installing heroku\n\n"
+npm install -g heroku
+if [ "$(heroku --version)" == "heroku/7.59.2 android-arm64 node-v17.4.0" ];then
+printf "\n Login heroku\n"
+heroku login -i
+else
+cd $PREFIX
+cd bin
+rm -f heroku > /dev/null 2>&1
+cd ..
+cd lib/node*
+rm -rf heroku > /dev/null 2>&1
+printf "\n Downloading heroku.deb\n"
+wget https://github.com/rooted-cyber/uploading/raw/main/heroku.deb
+printf "\n Installing heroku\n\n"
+dpkg -i heroku.deb
+fi
+}
+inh
+if [ "$(heroku --version)" == "heroku/7.59.2 android-arm64 node-v17.4.0" ];then
+echo
+else
+inh
+fi
