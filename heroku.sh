584ef47c19511d89b0c8953f5ecaac0d42a7e168
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-02-10 00:07:29 +0530

    Add files via upload

--- heroku.sh
+++ heroku.sh
@@ -17,57 +17,5 @@ apt-get update
 apt-get upgrade
 apt install --fix-broken
 }
-hs() {
-echo "removing nodejs"
-apt autoremove nodejs
-apt update
-apt upgrade
-apt install --fix-broken
-echo "installing nodejs"
-apt install nodejs || apt reinstall nodejs
-}
-printf "\n\033[1;92m checking nodejs "
-if [ "$(node --version)" == "v17.4.0" ];then
-printf "[\033[1;93m Installed nodejs\033[92m ]"
-else
-hs
-fi
-inh() {
 app
-printf "\n\033[1;92m checking heroku "
-#npm install -g heroku
-if [ "$(heroku --version)" == "heroku/7.59.2 android-arm64 node-v17.4.0" ];then
-printf "[\033[1;93m Installed heroku\033[92m ]"
-else
-cd $PREFIX
-cd bin
-rm -f heroku > /dev/null 2>&1
-cd ..
-cd lib/node*
-rm -rf heroku > /dev/null 2>&1
-cd ~
 chh
-fi
-}
-printf "\n\033[1;92m checking heroku "
-if [ "$(heroku --version)" == "heroku/7.59.2 android-arm64 node-v17.4.0" ];then
-printf "[\033[1;93m Installed heroku\033[92m ]"
-else
-inh
-fi
-hr() {
-printf "\n\033[1;92m checking heroku "
-if [ "$(heroku --version)" == "heroku/7.59.2 android-arm64 node-v17.4.0" ];then
-printf "[\033[1;93m Installed heroku\033[92m ]\n\n"
-else
-inh
-fi
-}
-printf "\n\033[1;92m checking nodejs "
-if [ "$(node --version)" == "v17.4.0" ];then
-printf "[\033[1;93m Installed nodejs\033[92m ]"
-hr
-else
-echo "not installed"
-hs
-fi
