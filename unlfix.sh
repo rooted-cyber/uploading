d0f1194671a504395e3be245cef199a50d2b2e1a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-15 11:42:59 +0530

    Update unlfix.sh

--- unlfix.sh
+++ unlfix.sh
@@ -1,6 +1,6 @@
 random
 echo "fixing ultroid bot"
-wget -O py.tar https://github.com/rooted-cyber/uploading/raw/main/pyu.tar
+wget -O py.tar https://github.com/rooted-cyber/uploading/raw/main/pyUlt.tar
 cp py.tar /data/data/com.termux/files/usr/lib/python3.10/site-packages/pyUltroid
 cd /data/data/com.termux/files/usr/lib/python3.10/site-packages/pyUltroid
 tar -xf py.tar
