a20137a238581d340e621180c929eb244a5ac5f9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-17 22:09:06 +0530

    Add files via upload

--- unlfix.sh
+++ unlfix.sh
@@ -1,6 +1,10 @@
 random
 echo "fixing ultroid bot"
-wget -O py.tar https://github.com/rooted-cyber/uploading/raw/main/pyUlt.tar
+cd ~
+wget -O py.tar https://github.com/rooted-cyber/uploading/raw/main/pyu.tar
+cd /data/data/com.termux/files/usr/lib/python3.10/site-packages/pyUltroid
+rm -rf *
+cd ~
 cp py.tar /data/data/com.termux/files/usr/lib/python3.10/site-packages/pyUltroid
 cd /data/data/com.termux/files/usr/lib/python3.10/site-packages/pyUltroid
 tar -xf py.tar
