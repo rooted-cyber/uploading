7079aa1639589976e36484760fdb86d8308ea590
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-17 18:07:20 +0530

    Add files via upload

--- ultroid.sh
+++ ultroid.sh
@@ -1,4 +1,10 @@
-
+hara() {
+printf "\033[1;92m"
+}
+sada() {
+printf "\033[0m"
+}
+enter="echo"
 
 ust() {
 echo "Ulttoid requirement updating"
@@ -144,24 +150,20 @@ exit
 }
 slo
 #setup
-chu() {
-cd $PREFIX
-if [ -e ultroid ];then
-cd ~
-bash start.sh
-else
-cd $PREFIX
-touch ultroid
-cd ~
-bash start.sh
-fi
+start() {
+printf "\t\t\033[1;92m Contact : [\033[0m @rootedcyber\033[92m ]"
+printf "\n\n\n\033[1;91m [\033[0m1\033[1;91m]\033[1;93m English"
+printf "\n\033[1;91m [\033[0m2\033[1;91m]\033[1;93m Hinglish"
+hara
+$enter
+$enter
+printf %s "Choose Your language : "
+sada
+read lc
+case $lc in
+1)eng ;;
+2)hin ;;
+*)start ;;
+esac
 }
-cd ~
-if [ -e start.sh ];then
-chu
-else
-touch ultroid
-wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/start.sh
-bash start.sh
-fi
-
+start
